3d37769908089f3630b0f6655fe26914d6e5c1ab
Francisco Soares <fmssn@cin.ufpe.br>
2022-03-08 18:10:42 -0300

    Criadas funções e IBAction

--- Caoculadora/Caoculadora/ViewController.swift
+++ Caoculadora/Caoculadora/ViewController.swift
@@ -8,11 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBAction func tocouMedio() {
+        print("Hello World")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func transformaAnosCaninosEmHumanos() {
+        // Lê o que está escrito no text field de anos
+        // Multiplica o valor escrito em anos por 7
+    }
+    
+    func transformaMesesCaninosEmHumanos() {
+        // Lê o que está escrito no text field de meses
+        // Multiplica o valor escrito em meses por 7 / 12
+    }
+    
+    func somaAnosTransformados() {
+        // soma anos humanos + meses humanos
+    }
+    
+    func escondeComponentes() {
+        // Esconde os labels, botões e text fields relativos a anos, meses e porte
+    }
+    
+    func exibeResultado() {
+        // Escreve na label de resultados o valor calculado em anos
+        // Exibe os labels de resultado e o botão de recãocular
+    }
 
 }
